83a6dedd0817c3611ab55c58f1121042727962fd
Robin Universe <mail@robinuniver.se>
2023-04-06 22:00:47 -0500

    Too Init To Quit

---
+++ ykk_bot.py
@@ -0,0 +1,18 @@
+import tweepy
+import json
+
+with open("config.json", "r") as jsonfile:
+    cfg = json.load(jsonfile)
+
+def main():
+    auth = tweepy.OAuthHandler( cfg['CONSUMER_API_KEY'], cfg['CONSUMER_API_SECRET'] )
+    auth.set_access_token( cfg['ACCESS_TOKEN'], cfg['ACCESS_SECRET'] )
+    api = tweepy.API(auth)
+
+    # Test Media Uploading.
+    media = api.media_upload("pfp.jpg")
+    tweet = "Welcome to the first test tweet of YKKBotV2! I'm writing this from scratch! if this works, hi, @RobinUniverse_!"
+    post_results = api.update_status( status=tweet, media_ids=[media.media_id] )
+
+if __name__ == "__main__":
+    main()
