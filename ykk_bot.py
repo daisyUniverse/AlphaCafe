4d45ac8410a1159fc24f88bacaf42daf422f79b9
Robin Universe <mail@robinuniver.se>
2023-04-07 10:55:20 -0500

    Removed freshness due to overcomplexity
    Trying to keep this as easy as possible for people to read, as I really like the idea of using this as a template

--- ykk_bot.py
+++ ykk_bot.py
@@ -47,32 +47,6 @@ def randomPanel():
             return os.path.join(dirpath, random_image)
     return None
 
-# Get Freshness of any given image - if this image is new, save a new freshness value of 10, but return 1.
-def GetFreshness(path):
-    with open("freshness.json", "r") as freshfile:
-        freshnessDB = json.load(freshfile)
-        
-    if path in freshnessDB:
-        freshness = freshnessDB[path]
-    else:
-        freshnessDB[path] = 10
-        with open("freshness.json", "w") as freshfile:
-            json.dump(freshnessDB, freshfile)
-            
-        freshness = 1
-    
-    return freshness
-
-# Set the freshness value of any given image
-def SetFreshness(path, freshness)
-    with open("freshness.json", "r") as freshfile:
-        freshnessDB = json.load(freshfile)
-        
-    freshnessDB[path] = freshness
-    
-    with open("freshness.json", "w") as freshfile:
-        json.dump(freshnessDB, freshfile)
-
 # Schedule the postTweet function to run every 30 minutes using the schedule library
 schedule.every(30).minutes.do(postTweet)
 
